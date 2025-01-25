e63059072597a47df8760342a082e21a065e887f
Luisa Souza <luisa.martins@benfatto.net.br>
2025-01-25 14:19:38 -0300

    [FEATURE-6] Styling page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {faCheck, faEdit, faSave, faTimes, faTrashAlt} from "@fortawesome/free-solid-svg-icons";
 
 @NgModule({
   declarations: [
@@ -16,9 +19,19 @@ import {FormsModule} from "@angular/forms";
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faCheck);
+    library.addIcons(faTrashAlt);
+    library.addIcons(faSave);
+    library.addIcons(faEdit);
+    library.addIcons(faTimes);
+  }
+}
